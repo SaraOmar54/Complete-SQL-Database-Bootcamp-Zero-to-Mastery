089c5696bfc0ef3ae5ef42b4833285352c1a3e65
Sara Omar <patagonia.dreamers@gmail.com>
2023-06-20 17:18:04 -0300

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -8,9 +8,18 @@
 * that are from either Japan or Australia
 */
 
+SELECT first_name, last_name, age, income
+FROM Customers
+WHERE income > 50000 AND (age < 30 AND age >= 50)
+AND (Country = 'Japan' OR Country ='Australia')
+
 /*
 * DB: Store
 * Table: Orders
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+
+SELECT SUM(totalamount) From Orders
+Where orderdate >2004-06-01 and orderdate=<2004-06030
+AND totalamount > 100;

33c0f946b63f1b5b41ce2e06e9956291370f0a1b
Sara Omar <patagonia.dreamers@gmail.com>
2023-06-23 13:47:24 -0300

    orderby.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -4,6 +4,8 @@
 * Question: Sort employees by first name ascending and last name descending
 */
 
+SELECT * FROM Employees
+order by First_name ASC, Last_name DESC;
 
 /*
 * DB: Employees
@@ -11,9 +13,14 @@
 * Question: Sort employees by age
 */
 
+SELECT * FROM Employees
+order by Birth_date;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+
+SELECT * FROM Employees
+WHERE first_name ILIKE 'K%' order by hire_date;
